66c796e79b7d512f66e6ddddecf1a34fc0badbd1
kot lena <aternetas@gmail.com>
2025-07-29 15:38:25 +0300

    init

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by f f on 29.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
