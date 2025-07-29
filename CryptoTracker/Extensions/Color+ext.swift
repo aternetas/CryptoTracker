2b4f63c67e57586f254265c5682eb633e5e42740
kot lena <aternetas@gmail.com>
2025-07-29 16:08:52 +0300

    add color resources

---
+++ CryptoTracker/Extensions/Color+ext.swift
@@ -0,0 +1,13 @@
+//
+//  Color+ext.swift
+//  CryptoTracker
+//
+//  Created by aternetas on 29.07.2025.
+//
+
+import SwiftUI
+
+extension Color {
+    static let white = Color(.white)
+    static let orange = Color(.orange)
+}
